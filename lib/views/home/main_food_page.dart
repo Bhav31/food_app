d1e626b529bdb7e4430046f8d29deb21b451c88f
Bhavani <bhavanimajoju3@gmail.com>
2023-10-19 17:40:20 +0530

    first commit

---
+++ lib/views/home/main_food_page.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:flutter/material.dart';
+import 'package:food_app/contrants/colors.dart';
+import 'package:food_app/views/widgets/bigText.dart';
+import 'package:food_app/views/widgets/smallText.dart';
+
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+      
+        children: [
+          Container(
+      child: Container(
+        margin: EdgeInsets.only(top: 45,bottom: 15),
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+               BigText(text: "Telangana",color: App.maincolor,),
+                Row(
+                  children: [
+                    SmallText(text: "Hyderabad",color: App.mainblackclr,),
+                    Icon(Icons.arrow_drop_down_rounded),
+                  ],
+                ),
+              ],
+            ),
+            Container(
+              width: 45,
+              height: 45,
+              child: Icon(Icons.search,color: Colors.white,),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: App.maincolor,
+              ),
+              
+            ),
+          ],
+        ),
+      ),
+    ),
+        ],
+      )
+    );
+  }
+}
---
+++ lib/views/widgets/smallText.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class SmallText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  double height;
+
+  SmallText(
+      {super.key,
+      this.color = const Color(0xFFccc7c5),
+      required this.text,
+      this.height = 1.2,
+      this.size = 12});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+          color: color, fontSize: size, 
+          fontFamily: "Roboto", 
+          height: height),
+    );
+  }
+}
